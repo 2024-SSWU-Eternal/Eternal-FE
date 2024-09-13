b4c2ba0e7a709da463162060b45fc25ea1fc1bf8
sujin <csujin2040@gmail.com>
2024-09-14 00:55:50 +0900

    fix : map 확대 축소 기능

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,15 +1,17 @@
-import React, { useState } from 'react';
-import { motion } from 'framer-motion';
+import React, { useState, useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import { useNavigate } from 'react-router-dom'
 import Btn from '../../assets/img/map/icon_back.png';
-import BoothMap from './BoothMap';
-import StampMap from './StampMap';
+import StampIcon from '../../assets/img/map/stamp-icon.svg';
+import MapImg from '../../assets/img/map/mapimg.png'
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+
 
 const Map = () => {
     const navigation = useNavigate();
     const [isExiting, setIsExiting] = useState(false);
-    const [isBooth, setIsBooth] = useState(true);
 
+    const [showTost, setShowTost] = useState(false);
     const back = () => {
         setIsExiting(true);
         setTimeout(() => {
@@ -17,26 +19,62 @@ const Map = () => {
         }, 400);
     };
 
-    const category = () => {
-        setIsBooth(!isBooth);
-    }
+    useEffect(() => {
+        setShowTost(true);
+        const timer = setTimeout(() => {
+            setShowTost(false);
+        }, 4000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
 
     return (
         <div className='Map_wrap container'>
             <header>
                 <div className="back_btn" onClick={back}><img src={Btn} alt="뒤로가기" /></div>
-                <div className="title">부스 정보</div>
+                <div className="title">축제 지도</div>
             </header>
-            <div className="cate">
-                <div className="booth">
-                    <div className={isBooth ? 'onToggle' : 'toggle'} onClick={category}>부스 지도</div>
+            <div className="wrap">
+                <div className="stamp-icon">
+                    <img src={StampIcon} alt="아이콘" />
+                    <p>스탬프 위치</p>
                 </div>
-                <div className="stamp">
-                    <div className={isBooth ? 'toggle' : 'onToggle'} onClick={category}>스탬프 지도</div>
+                <div className='mapImg'>
+                    <TransformWrapper
+                        initialScale={1}
+                        minScale={1}
+                        maxScale={10}
+                        centerContent={true}
+                    >
+                        <TransformComponent>
+                            <div>
+                                <img src={MapImg} />
+                            </div>
+
+                        </TransformComponent>
+                    </TransformWrapper>
                 </div>
+
+
+                <AnimatePresence>
+                    {showTost && (
+                        <motion.div
+                            className="tost"
+                            initial={{ opacity: 0, y: 50 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            exit={{ opacity: 0, y: 50 }}
+                            transition={{ duration: 0.4, ease: "easeInOut" }}
+                        >
+                            두 손가락으로 드래그하여 확대/축소를 할 수 있어요!
+                        </motion.div>
+                    )}
+                </AnimatePresence>
+
             </div>
 
-            {isBooth ? (<BoothMap />) : (<StampMap />)}
+
+
 
 
         </div>
