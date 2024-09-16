5e6935b6f5738aee4de525e9f6b2f791ba8628d6
jeongsaeyeong <ooooo0516@naver.com>
2024-09-16 11:10:09 +0900

    nav 수정 완

--- src/components/Section/Nav.jsx
+++ src/components/Section/Nav.jsx
@@ -1,20 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import { motion } from 'framer-motion';
-import HourGlass from '../../assets/img/main/hourglass.png';
-import Logo from '../../assets/img/main/Logo.png';
-import Glow02 from '../../assets/img/main/glow02.png';
-import Light from '../../assets/img/main/light02.png';
-import Banner01 from '../../assets/img/main/banner01.png'
 import { Link } from 'react-router-dom';
 import Footer from './Footer';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { clearTokens } from '../../store/authSlice';
+import NavContent from './NavContent';
 
 const Nav = () => {
     const [loginbtn, setLoginbtn] = useState(false);
     const token = localStorage.getItem('jwtToken')
     const isLoad = localStorage.getItem('loading');
-    const accessToken = useSelector((state) => state.auth.accessToken);
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -27,45 +22,13 @@ const Nav = () => {
     });
 
     const onLogout = () => {
-        dispatch(clearTokens);
-        localStorage.removeItem('token');
+        localStorage.clear();
     }
 
     return (
         <>
             {isLoad ? (
-                <>
-                    <div className='Nav_wrap'>
-                        {loginbtn ? (
-                            <div className='login' onClick={() => { onLogout(); }}>로그아웃</div>
-                        ) : (
-                            <Link to='/login' className='login'>로그인</Link>
-                        )}
-                        <div className='img_box'>
-                            <img className='hourclass' src={HourGlass} alt="hourglass" />
-                            <img className='hourclassglow' src={Glow02} alt="hourglass glow" />
-                            <img className='light01' src={Light} alt="light" />
-                            <img className='Logo' src={Logo} alt="logo" />
-                        </div>
-                        <div className="nav_box">
-                            <div className="top">
-                                <Link to='/timetable' className="time">타임테이블</Link>
-                                <Link to='/stamp' className="map">축제 배치도</Link>
-                                <Link to='/test' className="test">부스/푸드트럭 정보</Link>
-                            </div>
-                            <div className="bottom">
-                                <Link to='/announce/normal' className="announce">공지사항</Link>
-                                <Link to='/program' className="program">팜플렛</Link>
-                                <Link to='/reservation' className="goods">굿즈 예약</Link>
-                                <Link to='/test' className="test">유형 테스트</Link>
-                            </div>
-                        </div>
-                        <div className="banner">
-                            <Link to='/booth'><img src={Banner01} alt="" /></Link>
-                        </div>
-                    </div>
-                    <Footer />
-                </>
+                <NavContent onLogout={onLogout}  loginbtn={loginbtn}/>
             ) : (
                 <>
                     <motion.div
@@ -78,29 +41,11 @@ const Nav = () => {
                         ) : (
                             <Link to='/login' className='login'>로그인</Link>
                         )}
-                        <div className='img_box'>
-                            <img className='hourclass' src={HourGlass} alt="hourglass" />
-                            <img className='hourclassglow' src={Glow02} alt="hourglass glow" />
-                            <img className='light01' src={Light} alt="light" />
-                            <img className='Logo' src={Logo} alt="logo" />
-                        </div>
-                        <div className="nav_box">
-                            <div className="box">
-                                <Link to='/announce/normal' className="announce">공지사항</Link>
-                                <Link to='/timetable' className="time">타임테이블</Link>
-                                <Link to='/timetable' className="program">프로그램 소개</Link>
-                                <Link to='/program' className="goods">굿즈 예약</Link>
-                                <Link to='/stamp' className="map">축제 배치도</Link>
-                                <Link to='/test' className="test">유형 테스트</Link>
-                            </div>
-                        </div>
-                        <div className="banner">
-                            <Link to='/booth'><img src={Banner01} alt="" /></Link>
-                        </div>
+                        <NavContent onLogout={onLogout} loginbtn={loginbtn}/>
                     </motion.div>
-                    <Footer />
                 </>
             )}
+            <Footer />
         </>
 
     )
--- src/components/Section/NavContent.jsx
+++ src/components/Section/NavContent.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import Banner01 from '../../assets/img/main/banner01.png'
+import HourGlass from '../../assets/img/main/hourglass.png';
+import Logo from '../../assets/img/main/Logo.png';
+import Glow02 from '../../assets/img/main/glow02.png';
+import Light from '../../assets/img/main/light02.png';
+import { Link } from 'react-router-dom';
 
-const NavContent = () => {
+const NavContent = ({ onLogout, loginbtn }) => {
     return (
-        <div>NavContent</div>
+        <>
+            <div className='Nav_wrap'>
+                {loginbtn ? (
+                    <div className='login' onClick={() => { onLogout(); }}>로그아웃</div>
+                ) : (
+                    <Link to='/login' className='login'>로그인</Link>
+                )}
+                <div className='img_box'>
+                    <img className='hourclass' src={HourGlass} alt="hourglass" />
+                    <img className='hourclassglow' src={Glow02} alt="hourglass glow" />
+                    <img className='light01' src={Light} alt="light" />
+                    <img className='Logo' src={Logo} alt="logo" />
+                </div>
+                <div className="nav_box">
+                    <div className="top">
+                        <Link to='/timetable' className="time">타임테이블</Link>
+                        <Link to='/stamp' className="map">축제 배치도</Link>
+                        <Link to='/test' className="test">부스/푸드트럭 정보</Link>
+                    </div>
+                    <div className="bottom">
+                        <Link to='/announce/normal' className="announce">공지사항</Link>
+                        <Link to='/program' className="program">팜플렛</Link>
+                        <Link to='/reservation' className="goods">굿즈 예약</Link>
+                        <Link to='/test' className="test">유형 테스트</Link>
+                    </div>
+                </div>
+                <div className="banner">
+                    <Link to='/booth'><img src={Banner01} alt="" /></Link>
+                </div>
+            </div>
+        </>
     )
 }
 
