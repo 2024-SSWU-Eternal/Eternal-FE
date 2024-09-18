80f6c3260a0e6e267b6f235a3a0135e332666420
jeongsaeyeong <ooooo0516@naver.com>
2024-09-18 20:34:26 +0900

    디저트랑 scroll 넣어놨어요

--- src/components/FoodInfo/DessertDetail.jsx
+++ src/components/FoodInfo/DessertDetail.jsx
@@ -1,16 +1,45 @@
 import React from 'react'
 
 const DessertDetail = () => {
-  return (
-    <div className='DessertDetail_wrap '>
-      <div className="table">
-            <div className="food_name">아메리카노</div>
-            <div className="ingredients">
-                <p>정제수, 커피추출액(아라비카 100%) </p>
+    return (
+        <div className='DessertDetail_wrap '>
+            <div className="table">
+                <div className="food_name">츄러스</div>
+                <div className="ingredients">
+                    <p>
+                        밀가루(호주산,미국산), 옥수수전분(러시아,헝가리산,세르비아산)혼합분유(네덜란드)<br />
+                        혼합제(탄산수소나트륨,글루코노데타락톤,주석산수소칼륨,제일인산칼슘,푸마르산,옥수수전분,스테아린칼슘,자당지방산에스테르),정제소금,기타가공품,코코넛분말,전란분,찹쌀가루,곡류가공품,향료제제(바닐린,포도당,천연향료)<br />
+                        혼합제2 ㅡ중쇄중성지방유,정제팜유,합성향료<br />
+                        혼합제3 ㅡ대두)
+                    </p>
+                </div>
+            </div>
+            <div className="table">
+                <div className="food_name">아이스크림</div>
+                <div className="ingredients">
+                    <p>
+                        물,정백당,물엿,유크림(국산),혼합분유(네덜란드산/탈지분유,유청)요긴(이탈리아산),가당연유,텍스트린,글리세린지방산에스테르,로커스트콩검,카르복시메틸셀룸로오스나트륨,구아검,카라기난,알긴산나트륨,포도당,나닐린,구연산
+                    </p>
+                </div>
+            </div>
+            <div className="table">
+                <div className="food_name">회오리감자</div>
+                <div className="ingredients">
+                    <p>
+                        감자80%,정제수,대두유5%, 밀가루(소맥분)4.587%,카사바전분0.438%,옥수수가루0.35(유전자변형옥수수포함가능성있음),쌀가루0.253%젱제소금,건마늘분말, 후추분말, 치자황색소,대두및힘유
+                    </p>
+                </div>
+            </div>
+            <div className="table">
+                <div className="food_name">구름파르페</div>
+                <div className="ingredients">
+                    <p>
+                        정제수,우유,설탕,과당,말토덱스트린,요구르트분말,산도조절제,구아검,이산화규소,잔탄검,덱스트린,유당,합성향료,천연향료,설탕,클리어오렌지농축액(이스라엘산),식용색소,보존료,산도조절제,레몬
+                    </p>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default DessertDetail
