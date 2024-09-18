d3f1c7a93021dd0689442451a0862fd4784768d8
guswlsl <111550606+guswlsl@users.noreply.github.com>
2024-09-18 21:31:23 +0900

    푸드트럭 식사 추가

--- src/components/FoodInfo/FoodDetail.jsx
+++ src/components/FoodInfo/FoodDetail.jsx
@@ -1,14 +1,67 @@
-import React from 'react'
+import React from 'react';
 
 const FoodDetail = () => {
   return (
     <div className='FoodDetail_wrap '>
       <div className="table">
-            <div className="food_name">타코야끼</div>
-            <div className="ingredients">
-                <p>밀가루, 문어, 설탕, 소금, 생강가루, 계란, 옥수수전분, 가다랑어포, 데리야끼소스, 매운데리야끼소스, 치즈드레싱소스, 마요네즈</p>
-            </div>
+        <div className="food_name">소고기불초밥, 새우불초밥, 불초밥세트</div>
+        <div className="ingredients">
+          <p>소고기부챗살, 초새우, 쌀, 초밥용소스, 데리야끼소스, 스위트 칠리소스,
+            마요네즈, 생와사비, 적초생강</p>
         </div>
+
+        <div className="food_name">유부초밥</div>
+        <div className="ingredients">
+          <p>쌀, 초밥용소스, 단무지, 유부(대두, 식물성유지(유채유, 대두유, 옥수수유),
+            염화마그네슘, 염화칼슘, 설탕, 간장(대두, 밀, 정제소금, 주정, 양조식초,
+            정제소금, 카라멜색소, 초산나트륨, 변성전분, L-글루탐산나트륨, 정제수)</p>
+        </div>
+
+        <div className="food_name">닭강정</div>
+        <div className="ingredients">
+          <p>닭정육, 콩기름, 정제수, 밀가루, 옥수수전분, 매운맛조미분, 블랙페퍼, 양파분말, 올레오레진캡시컴</p>
+        </div>
+
+        <div className="food_name">스테이크1</div>
+        <div className="ingredients">
+          <p>토시살, 빵(밀가루), 감자, 콩식용유, 스테이크 시즈닝(정제소금, 양념류(흑후추, 붉은고추), 마늘, 해바라기유, 천연향(캐러웨이, 골파, 서양자초, 파슬리, 세이지, 세이보리, 타임), 파프리카추출물), 스테이크소스, 크림소스(야자정재유, 탈지분유, 가공버터, 우유, 대두유, 전지분유, 유크림) 생강</p>
+        </div>
+
+        <div className="food_name">스테이크2</div>
+        <div className="ingredients">
+          <p>토시살, 야채(당근, 브루컬리, 컬리플라워, 옐로우캐럿), 스테이크 시즈닝(정제소금, 양념류(흑후추, 붉은고추), 마늘, 해바라기유, 천연향(캐러웨이, 골파, 서양자초, 파슬리, 세이지, 세이보리, 타임), 파프리카추출물), 스테이크소스</p>
+        </div>
+
+        <div className="food_name">감자튀김</div>
+        <div className="ingredients">
+          <p>감자, 콩식용유, 체다치즈소스</p>
+        </div>
+
+        <div className="food_name">두부스테이크</div>
+        <div className="ingredients">
+          <p>두부, 콩식용유, 두부스테이크소스(토마토소스, 설탕, 간장), 샐러드(양배추, 적색양배추, 양상추), 참깨소스(정제수, 옥수수기름, 조간장, 발효식초, 참깨)</p>
+        </div>
+
+        <div className="food_name">야끼소바</div>
+        <div className="ingredients">
+          <p>밀가루, 글루텐, 대두레시틴, 젖산, 젖산나트륨, 글루코노델타락톤, 유채유, 난백, 치자황색소, 정제소금, 탄산나트륨, 정제수, 설탕, 양조간장, 천일염, 치킨 엑스S, 발효식초, 카라멜색소, L-글루탐산나트륨제제, 잔탄검, 향미증진제, 과일 및 야채(토마토, 양파, 사과, 대추야자, 당근, 복숭아, 오렌지, 마늘), 포도당과당시럽, 당밀, 굴추출물, 다시마추출물, 타마린드검, 구아검, 효모추출물, 닭고기추출물, 돼지고기추출물, 가수분해 콩 단백질, 우스타소스, 생강, 감초 분말, 계피, 가다랑어포, 마요네즈</p>
+        </div>
+
+        <div className="food_name">타코야끼</div>
+        <div className="ingredients">
+          <p>밀가루, 문어, 설탕, 소금, 생강가루, 계란, 옥수수전분, 가다랑어포, 데리야끼소스, 매운데리야끼소스, 치즈소스, 마요네즈</p>
+        </div>
+
+        <div className="food_name">소떡소떡</div>
+        <div className="ingredients">
+          <p>쌀떡볶이, 숯불갈비향소시지(돼지고기, 당근, 대두단백), 비프스테이크소스, 숯불갈비맛 엑기스</p>
+        </div>
+
+        <div className="food_name">닭꼬치</div>
+        <div className="ingredients">
+          <p>닭다리살(브라질산), 대파(국내산), 떡(국내산), 고추장소스, 데리야끼소스, 소금</p>
+        </div>
+      </div>
     </div>
   )
 }
