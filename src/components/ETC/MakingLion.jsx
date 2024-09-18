8c2dd0302081d16262f7db0fd283509f97006812
nakyeong <yunna3425@naver.com>
2024-09-19 02:45:22 +0900

    feat: 만든 이들 담당 파트 추가

--- src/components/ETC/MakingLion.jsx
+++ src/components/ETC/MakingLion.jsx
@@ -1,46 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const MakingLion = ({ title, data }) => {
+    const [click, setClick] = useState([]);
+
+    const clickLion = (index) => {
+        setClick((clickIndex) => {
+            if (clickIndex.includes(index)) {
+                return clickIndex.filter((i) => i !== index);
+            } else {
+                return [...clickIndex, index];
+            }
+        });
+    };
+
+
     return (
         <div>
             <h5>{title}</h5>
             <div className='lion_wrap'>
                 {Array.isArray(data) ? (
-                    data.map((list, key) => (
-                        <div className="lion" key={key}>
+                    data.map((list, index) => (
+                        <div
+                            className={`lion ${click.includes(index) ? 'active' : ''}`}
+                            key={index}
+                            onClick={() => clickLion(index)}
+                        >
                             <div className="text">
                                 <p className="organization">{list.organization}</p>
                                 <p className="name">{list.name}</p>
                                 <p className="department">{list.department}</p>
-                                <p className="desc">
-                                    {Array.isArray(list.desc) 
-                                        ? list.desc.map((line, index) => (
-                                            <span key={index}>
-                                                {line}
-                                                <br />
-                                            </span>
-                                        ))
-                                        : list.desc
-                                    }
-                                </p>
                             </div>
                             <img src={list.img} alt="" />
+                            <div className='role'>
+                                <p>담당 파트</p>
+                                <p className="desc">{list.desc}</p>
+                            </div>
                         </div>
                     ))
                 ) : (
-                    <div className="lion">
+                    <div
+                        className={`lion ${click.includes(0) ? 'active' : ''}`}
+                        onClick={() => clickLion(0)}
+                    >
                         <div className="text">
                             <p className="organization">{data.organization}</p>
                             <p className="name">{data.name}</p>
                             <p className="department">{data.department}</p>
-                            <p className="desc">{data.desc}</p>
                         </div>
                         <img src={data.img} alt="" />
+                        <div className='role'>
+                            <p>담당 파트</p>
+                            <p className="desc">{data.desc}</p>
+                        </div>
                     </div>
                 )}
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default MakingLion
