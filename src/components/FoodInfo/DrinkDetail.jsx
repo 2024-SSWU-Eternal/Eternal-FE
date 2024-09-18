fda2ef278bc860c94bb6495e225d25ac20d57935
nakyeong <yunna3425@naver.com>
2024-09-18 23:51:33 +0900

    feat: 푸드트럭 음료 정보 추가

--- src/components/FoodInfo/DrinkDetail.jsx
+++ src/components/FoodInfo/DrinkDetail.jsx
@@ -3,10 +3,172 @@ import React from 'react'
 const DrinkDetail = () => {
   return (
     <div className='DrinkDetail_wrap '>
-      <div className="table">
+        <div className="table">
+            <div className="food_name">아메리카노</div>
+            <div className="ingredients">
+                <p>정제수, 커피원두(브라질산 85%, 콜롬비아산 15%)</p>
+            </div>
+        </div>
+        <div className="table">
+            <div className="food_name">카페라떼</div>
+            <div className="ingredients">
+                <p>정제수, 커피원두(브라질산 85%, 콜롬비아산 15%), 우유</p>
+            </div>
+        </div>
+        <div className="table">
+            <div className="food_name">바닐라라떼</div>
+            <div className="ingredients">
+                <p>정제수, 커피원두(브라질산 85%, 콜롬비아산 15%), 우유, 설탕, 천연향료(바닐라향), 카라멜색소, 혼합제제, 산도조절제</p>
+            </div>
+        </div>
+        <div className="table">
+            <div className="food_name">코코넛라떼</div>
+            <div className="ingredients">
+                <p>정제수, 커피원두(브라질산 85%, 콜롬비아산 15%), 우유, 사탕수수설탕, 천연향료(코코넛향)</p>
+            </div>
+        </div>
+        <div className="table">
+            <div className="food_name">버터카라멜라떼</div>
+            <div className="ingredients">
+                <p>정제수, 커피원두(브라질산 85%, 콜롬비아산 15%), 우유, 설탕, 천연버터스카치향, 카라멜색소, 구연산보존료</p>
+            </div>
+        </div>
+        <div className="table">
+            <div className="food_name">망고복숭아아이스티</div>
+            <div className="ingredients">
+                <p>정제수, 망고, 설탕, 망고퓨레, 기타과당, 혼합제제, 구연산, 복숭아농축액, 카라멜색소, 홍차추출분말</p>
+            </div>
+        </div>
+        <div className="table">
             <div className="food_name">아샷추</div>
             <div className="ingredients">
-                <p>정제수, 커피추출액(아라비카 100%), 백설탕, 홍차추출분말, 복숭아향(합성착향료), 구연산, 스테비올배당체, 복숭아과즙분말, 복숭아 </p>
+                <p>정제수, 커피원두(브라질산 85%, 콜롬비아산 15%), 백설탕, 구연산, 복숭아농축액, 카라멜색소, 홍차추출분말</p>
+            </div>
+        </div>
+        <div className="table">
+            <div className="food_name">히비스커스레몬차</div>
+            <div className="ingredients">
+                <p>정제수, 히비스커스잎, 레몬, 설탕, 사양벌꿀, 구연산, 혼합제제, 정제소금, 효소처리스테비아, 천연향료</p>
+            </div>
+        </div>
+        <div className="table">
+            <div className="food_name">카모마일유자차</div>
+            <div className="ingredients">
+                <p>정제수, 카모마일, 유자절편, 설탕, 기타과당, 사과산</p>
+            </div>
+        </div>
+        <div className="table">
+            <div className="food_name">크림핫초코</div>
+            <div className="ingredients">
+                <p>정제수, 백설탕, 식물성크림(물엿, 식물성유지), 제이인산칼륨, 카제인나트륨, 탄산칼슘, 유청분말, 코코아분말, 정제소금, 우유, 휘핑혼합유, 야자유, 설탕, 이산화질소, 유화제, 합성향료(바닐라, 치즈향), 정제소금</p>
+            </div>
+        </div>
+        <div className="table">
+            <div className="food_name">블루레몬에이드</div>
+            <div className="ingredients">
+                <p>정제수, 이산화탄소, 합성향료(씨트러스향), 구연산, 클리어오렌지농축액(이스라엘산), 식용색소, 보존료, 산도조절제, 레몬, 설탕, 사양벌꿀, 구연산, 혼합제제, 정제소금, 효소처리스테비아, 천연향료(오렌지향)</p>
+            </div>
+        </div>
+        <div className="table">
+            <div className="food_name">체리레몬에이드</div>
+            <div className="ingredients">
+                <p>정제수, 이산화탄소, 합성향료(씨트러스향), 구연산, 설탕, 체리(칠레산), 체리농축액, 레몬, 설탕, 사양벌꿀, 구연산, 혼합제제, 정제소금, 효소처리스테비아, 천연향료</p>
+            </div>
+        </div>
+        <div className="table">
+            <div className="food_name">한라봉에이드</div>
+            <div className="ingredients">
+                <p>정제수, 이산화탄소, 합성향료(씨트러스향), 구연산, 구연산삼나트륨, 프로필렌글리콜, 한라봉, 정백당</p>
+            </div>
+        </div>
+        <div className="table">
+            <div className="food_name">리얼딸기라떼</div>
+            <div className="ingredients">
+                <p>정제수, 우유, 딸기, 백설탕</p>
+            </div>
+        </div>
+        <div className="table">
+            <div className="food_name">크림초코라떼</div>
+            <div className="ingredients">
+                <p>정제수, 우유, 카제인나트륨, 탄산칼슘, 유청분말, 코코아분말, 정제소금, 휘핑혼합유, 야자유, 설탕, 아산화질소, 유화제, 합성향료(바닐라, 치즈향), 정제소금</p>
+            </div>
+        </div>
+        <div className="table">
+            <div className="food_name">구름파르페</div>
+            <div className="ingredients">
+                <p>정제수, 우유, 설탕, 과당, 말토덱스티린, 요구르트분말, 산도조절제, 구아검, 이산화규소, 잔탄검, 덱스트린, 유당, 합성향료, 천연향료, 설탕, 클리어오렌지농축액(이스라엘산), 식용색소, 보존료, 산도조절제, 레몬</p>
+            </div>
+        </div>
+        <div className="table">
+            <div className="food_name">깔라만시레몬주스</div>
+            <div className="ingredients">
+                <p>정제수, 깔라만시원액, 레몬, 설탕, 사양벌꿀, 구연산, 혼합제제, 정제소금, 효소처리스테비아, 천연향료(오렌지향)</p>
+            </div>
+        </div>
+        <div className="table">
+            <div className="food_name">파라다이스주스</div>
+            <div className="ingredients">
+                <p>정제수, 오렌지, 키위, 파인애플, 망고, 설탕</p>
+            </div>
+        </div>
+        <div className="table">
+            <div className="food_name">망고봉봉</div>
+            <div className="ingredients">
+                <p>정제수, 망고, 설탕, 우유, 휘핑혼합유, 야자유, 설탕, 이산화질소, 유화제, 합성향료(바닐라, 치즈향), 정제소금</p>
+            </div>
+        </div>
+        <div className="table">
+            <div className="food_name">라임애플마티니</div>
+            <div className="ingredients">
+                <p>정제수, 이산화탄소, 합성향료(씨트러스향), 설탕, 레몬주스, 라임주스, 포도당시럽, 구연산, 합성향료, 사과퓨레, 청사과농축액, 산도조절제, 보존료, 혼합제제, 식용색소</p>
+            </div>
+        </div>
+        <div className="table">
+            <div className="food_name">선셋레몬</div>
+            <div className="ingredients">
+                <p>정제수, 이산화탄소, 합성향료(씨트러스향), 설탕, 사워체리주스, 구연산, 당근추출물, 합성향료(신체리향), 포도당시럽, 레몬주스, 천연향료, 비타민C</p>
+            </div>
+        </div>
+        <div className="table">
+            <div className="food_name">피치트리</div>
+            <div className="ingredients">
+                <p>정제수, 이산화탄소, 합성향료(씨트러스향), 설탕, 포도당시럽, 복숭아주스, 구연산, 합성향료, 카로틴, 비타민C, 히비스커스</p>
+            </div>
+        </div>
+        <div className="table">
+            <div className="food_name">블루오션레몬</div>
+            <div className="ingredients">
+                <p>정제수, 이산화탄소, 합성향료(씨트러스향), 구연산, 클리어오렌지농축액(이스라엘산), 식용색소, 보존료, 포도당시럽, 레몬주스, 천연향료, 비타민C</p>
+            </div>
+        </div>
+        <div className="table">
+            <div className="food_name">라임모히또</div>
+            <div className="ingredients">
+                <p>정제수, 이산화탄소, 합성향료(씨트러스향), 설탕, 레몬주스, 라임주스, 포도당시럽, 구연산, 레몬농축액, 산도조절제, 보존료, 유화제, 식용색소</p>
+            </div>
+        </div>
+        <div className="table">
+            <div className="food_name">진저에일</div>
+            <div className="ingredients">
+                <p>정제수, 이산화탄소, 합성향료(씨트러스향), 생강농축액, 기타과당, 설탕, 당적생강, 생강, 레몬농축액, 산도조절제, 벌꿀, 합성향료, 잔탄검, 식용색소</p>
+            </div>
+        </div>
+        <div className="table">
+            <div className="food_name">선라이즈자몽</div>
+            <div className="ingredients">
+                <p>정제수, 이산화탄소, 합성향료(씨트러스향), 설탕, 핑크자몽주스, 천연향료, 구연산, 포토과피색소, 비타민C</p>
+            </div>
+        </div>
+        <div className="table">
+            <div className="food_name">피나콜라다</div>
+            <div className="ingredients">
+                <p>정제수, 이산화탄소, 합성향료(씨트러스향), 설탕, 과당, 산도조절제, 파인애플크러쉬, 파인애플농축액, 잔탄검, 감미료, 정제소금, 보존료, 식용색소</p>
+            </div>
+        </div>
+        <div className="table">
+            <div className="food_name">깔루아밀크</div>
+            <div className="ingredients">
+                <p>정제수, 우유, 물엿, 설탕, 기타과당, 커피농축액, 혼합제제, 합성향료, 보존료, 산도조절제</p>
             </div>
         </div>
     </div>
