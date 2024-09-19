59b4c9773cb1756d4942ae53d6596232a7a103a8
jeongsaeyeong <ooooo0516@naver.com>
2024-09-19 18:52:58 +0900

    member 수정

--- src/components/ETC/member.js
+++ src/components/ETC/member.js
@@ -95,171 +95,171 @@ export const backEnd = [
 
 export const sswuleader = [
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "제36대 총학생회 ‘여일하게'",
         name: '임수빈',
         department: '법학부'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "제36대 총학생회 ‘여일하게'",
         name: '박세은',
         department: '법학부'
     }
 ]
 
 export const festivalleader = {
-    organization: "제 36대 총학생회 ‘여일하게'",
+    organization: "제36대 총학생회 ‘여일하게'",
     name: '피정빈',
     department: '경제학과'
 }
 
 export const festivalmemeber = [
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "제36대 총학생회 ‘여일하게'",
         name: '홍은영',
         department: '정치외교학과'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "제36대 총학생회 ‘여일하게'",
         name: '엄채연',
         department: '서양화과'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "제36대 총학생회 ‘여일하게'",
         name: '최희영',
         department: '소비자생활문학산업학과'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "제36대 총학생회 ‘여일하게'",
         name: '조현서',
         department: '국어국문학과'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "제36대 총학생회 ‘여일하게'",
         name: '오나연',
         department: '문화예술경영학과'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "제36대 총학생회 ‘여일하게'",
         name: '이은재',
         department: '인문융합예술계열'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "제36대 총학생회 ‘여일하게'",
         name: '임채민',
         department: '문화예술경영학과'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "제36대 총학생회 ‘여일하게'",
         name: '김나현',
         department: '뷰티산업학과'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "제36대 총학생회 ‘여일하게'",
         name: '정유민',
         department: '일본어문・문화학과'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "제36대 총학생회 ‘여일하게'",
         name: '이경민',
         department: '일본어문・문화학과'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "제36대 총학생회 ‘여일하게'",
         name: '김세진',
         department: '법학부'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "제36대 총학생회 ‘여일하게'",
         name: '정다희',
         department: '현대실용음악학과'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "제36대 총학생회 ‘여일하게'",
         name: '박소유',
         department: '융합보안공학과'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "제36대 총학생회 ‘여일하게'",
         name: '신지수',
         department: '국어국문학과'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "제36대 총학생회 ‘여일하게'",
         name: '박유채',
         department: '서비스・디자인공학과'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "제36대 총학생회 ‘여일하게'",
         name: '허수빈',
         department: '미디어커뮤니케이션학과'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "제36대 총학생회 ‘여일하게'",
         name: '최인애',
         department: '미디어커뮤니케이션학과'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "제36대 총학생회 ‘여일하게'",
         name: '전여원',
         department: '뷰티산업학과'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "제36대 총학생회 ‘여일하게'",
         name: '박수아',
         department: '경영학부'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "제36대 총학생회 ‘여일하게'",
         name: '이선민',
         department: '법학부'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "제36대 총학생회 ‘여일하게'",
         name: '윤수민',
         department: '디자인과'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "제36대 총학생회 ‘여일하게'",
         name: '김미연',
         department: '사회과학계열'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "제36대 총학생회 ‘여일하게'",
         name: '홍영인',
         department: '문화예술경영학과'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "제36대 총학생회 ‘여일하게'",
         name: '신채연',
         department: '법학부'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "제36대 총학생회 ‘여일하게'",
         name: '이현지',
         department: '융합보안공학과'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "제36대 총학생회 ‘여일하게'",
         name: '김윤서',
         department: '사회과학계열'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "제36대 총학생회 ‘여일하게'",
         name: '손예빈',
         department: '바이오식품공학과'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "2024 수정대동제 축제기획단",
         name: '김채연',
         department: '디자인과'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "2024 수정대동제 축제기획단",
         name: '민서현',
         department: '미디어커뮤니케이션학과'
     },
     {
-        organization: "제 36대 총학생회 ‘여일하게'",
+        organization: "2024 수정대동제 축제기획단",
         name: '이솔희',
         department: '디자인과'
     },
